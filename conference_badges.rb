d3c2f283f8a5202be8ba6e624663392b588433fb
Jenny Kim <kimj1905@gmail.com>
2021-02-27 21:00:52 -0500

    commit tree 5ce232b1bb2a417ec0ab3d7fc3bec338c1e37307

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.collect do |speaker|
+        badge_maker(speaker)
+    end
+end
+
+def assign_rooms(attendees)
+    attendees.collect.with_index do |speaker, index|
+        index += 1
+        "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    
+    assign_rooms(attendees).each do |room|
+        puts room
+    end
+end
